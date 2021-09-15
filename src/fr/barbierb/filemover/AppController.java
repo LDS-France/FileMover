d6faf1a69b24eb050ec9047ace47d7792e886d6a
barbierb <barbierb@CFR282092.lenze.com>
2021-09-15 08:47:51 +0200

    add duration of files
    Signed-off-by: barbierb <barbierb@CFR282092.lenze.com>

---
+++ src/fr/barbierb/filemover/AppController.java
@@ -0,0 +1,204 @@
+package fr.barbierb.filemover;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Properties;
+
+import javax.swing.SwingUtilities;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+
+public class AppController {
+
+	@FXML private Button button_src;
+	@FXML private Button button_dest;
+	@FXML private TextField textfield_extension;
+	@FXML private ChoiceBox<String> combobox_freq;
+	@FXML private TextField textfield_lifetime;
+	@FXML private CheckBox checkbox_delete;
+
+	String fileLifetime;
+	String fileExt;
+	int comboboxIndex;
+	String folder_src;
+	String folder_dest;
+
+	Thread fileMoverThread;
+
+	private Properties prop;
+
+	@FXML
+	void initialize() {
+		
+		prop = new Properties();
+		try {
+			FileReader reader = new FileReader("app.properties");
+			prop.load(reader);
+		} catch (Exception e1) {
+			System.err.println("Le fichier 'app.properties' n'existait pas.");
+		}
+
+		fileExt = (String) prop.getProperty("fileExt", null);
+		fileLifetime = (String) prop.getProperty("fileLifetime", null);
+		folder_src = (String) prop.getProperty("folder_src", null);
+		folder_dest = (String) prop.getProperty("folder_dest", null);
+		comboboxIndex = Integer.parseInt(prop.getProperty("comboboxIndex", "2"));
+		
+		if(folder_dest != null && folder_dest.equals("delete")) {
+			button_dest.setText("Suppression");
+			button_dest.setDisable(true);
+		}
+
+		Runnable task = new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					if(folder_src != null && folder_dest != null && fileExt != null) {
+						File src = new File(folder_src);
+						File dest = new File(folder_dest);
+
+						if(src.isDirectory() && dest.isDirectory()) {
+							
+							int fileMovedAmount = 0;
+							
+							File files[] = src.listFiles();
+							for(int idx = 0; idx < files.length; idx++) {
+
+								File file = files[idx];
+								if(!file.getName().endsWith(fileExt))
+									continue;
+
+								try {
+									if(checkbox_delete.isSelected()) {
+										file.delete();
+										continue;
+									}
+									
+									File destFile = new File(dest.getAbsoluteFile()+File.separator+file.getName());
+									if(destFile.exists())
+										continue;
+									
+									Files.move(file.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+									fileMovedAmount++;
+									System.out.println(file.toPath()+" copied to "+dest.toPath());
+								} catch (IOException e) {
+									System.out.println("ERREUR");
+									e.printStackTrace();
+								}
+							}
+							
+							if(fileMovedAmount>0) {
+								String s = ((fileMovedAmount>1)?"s":"");
+								String notifMessage = fileMovedAmount+" fichier"+s+" déplacé"+s+".";
+								SwingUtilities.invokeLater(() -> Main.trayIcon.displayMessage("FileMover", notifMessage, java.awt.TrayIcon.MessageType.INFO));
+							}
+
+						}
+					}
+					
+					long timeToSleep = 0;
+					if(comboboxIndex == 0)
+						timeToSleep = 1000 * 60 * 30; // 30 mins
+					else if(comboboxIndex == 1)
+						timeToSleep = 1000 * 60 * 60; // 1 h
+					else if(comboboxIndex == 2)
+						timeToSleep = 1000 * 60 * 120; // 2 h
+					else if(comboboxIndex == 3)
+						timeToSleep = 1000 * 60 * 240; // 4 h
+						
+					
+					try {
+						Thread.sleep(timeToSleep);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+
+		fileMoverThread = new Thread(task);
+
+		combobox_freq.getItems().addAll("30mins", "1h", "2h", "4h");
+		combobox_freq.getSelectionModel().select(comboboxIndex);
+		combobox_freq.getSelectionModel().selectedIndexProperty().addListener((obs, old, news) -> {
+			comboboxIndex = news.intValue();
+			System.out.println("new comboboxIndex : "+comboboxIndex);
+			prop.setProperty("comboboxIndex", comboboxIndex+"");
+			save();
+		});
+		
+		textfield_lifetime.setText(fileLifetime==null?"":fileLifetime);
+
+		textfield_extension.setText(fileExt==null?"":fileExt);
+		textfield_extension.textProperty().addListener((obs, old, news) -> {
+			fileExt = news;
+			System.out.println("new ext : "+fileExt);
+			prop.setProperty("fileExt", fileExt);
+			save();
+		});
+
+		button_src.setText(folder_src==null?"Dossier source non défini":folder_src);
+		button_src.setOnMousePressed(e -> {
+			DirectoryChooser chooser = new DirectoryChooser();
+			chooser.setTitle("Select the destination folder.");
+
+			File folder = chooser.showDialog(Main.stage);
+			if (folder != null) {
+				System.out.println("new src folder : "+folder.getAbsolutePath());
+				folder_src = folder.getAbsolutePath();
+				button_src.setText(folder_src);
+				prop.setProperty("folder_src", folder_src);
+				save();
+			}
+		});
+
+		button_dest.setText(folder_dest==null?"Dossier destination non défini":folder_dest);
+		button_dest.setOnMousePressed(e -> {
+			DirectoryChooser chooser = new DirectoryChooser();
+			chooser.setTitle("Select the destination folder.");
+
+			File folder = chooser.showDialog(Main.stage);
+			if (folder != null) {
+				System.out.println("new dest folder : "+folder.getAbsolutePath());
+				folder_dest = folder.getAbsolutePath();
+				button_dest.setText(folder_dest);
+				prop.setProperty("folder_dest", folder_dest);
+				save();
+			}
+		});
+		
+		checkbox_delete.selectedProperty().addListener((obs, old, newv) -> {
+			if(newv) {
+				folder_dest = "delete";
+				prop.setProperty("folder_dest", folder_dest);
+				button_dest.setText("Suppression");
+				button_dest.setDisable(true);
+				save();
+			} else {
+				button_dest.setText("Dossier destination non défini");
+				button_dest.setDisable(false);
+			}
+		});
+
+		fileMoverThread.start();
+	}
+
+	private void save() {
+		try {
+			FileWriter fw = new FileWriter("app.properties");
+			prop.store(fw, "");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
---
+++ src/fr/barbierb/filemover/Main.java
@@ -0,0 +1,121 @@
+package fr.barbierb.filemover;
+
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.StandardOpenOption;
+
+import javax.imageio.ImageIO;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	static Stage stage;
+	static TrayIcon trayIcon;
+
+	@Override
+	public void start(Stage s) throws Exception {
+		Main.stage = s;
+		addAppToTray();
+
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(getClass().getResource("/fxml/App.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Platform.setImplicitExit(false);
+
+		s.setTitle("FileMover - Lenze Ruitz");
+		s.setScene(new Scene(root));
+		s.setMinWidth(400);
+		s.setMinHeight(250);
+		s.setFullScreenExitHint("");
+		s.setResizable(false);
+		s.getIcons().add(new Image(getClass().getResourceAsStream("/img/icon_black.png")));
+		s.show();
+	}
+
+	private void addAppToTray() {
+		try {
+			java.awt.Toolkit.getDefaultToolkit();
+
+			if (!SystemTray.isSupported()) {
+				System.out.println("No system tray support, application exiting.");
+				return;
+			}
+			
+			SystemTray tray = SystemTray.getSystemTray();
+
+			java.awt.Image image = ImageIO.read(getClass().getResourceAsStream("/img/icon_white.png"));
+			trayIcon = new TrayIcon(image);
+
+			trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+			java.awt.MenuItem openItem = new java.awt.MenuItem("Propriétés");
+			openItem.addActionListener(event -> Platform.runLater(this::showStage));
+
+			java.awt.Font defaultFont = java.awt.Font.decode(null);
+			java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+			openItem.setFont(boldFont);
+
+			java.awt.MenuItem exitItem = new java.awt.MenuItem("Quitter");
+			exitItem.addActionListener(event -> {
+				Platform.exit();
+				tray.remove(trayIcon);
+				System.exit(0);
+			});
+
+			final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+			popup.add(openItem);
+			popup.addSeparator();
+			popup.add(exitItem);
+			trayIcon.setPopupMenu(popup);
+
+			tray.add(trayIcon);
+		} catch (java.awt.AWTException | IOException e) {
+			System.out.println("Unable to init system tray");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Shows the application stage and ensures that it is brought ot the front of all stages.
+	 */
+	private void showStage() {
+		if (stage != null) {
+			stage.show();
+			stage.toFront();
+		}
+	}
+
+	public static void main(String[] args) {
+		File file = new File("filemover.lock");
+		try {
+		    FileChannel fc = FileChannel.open(file.toPath(),
+		            StandardOpenOption.CREATE,
+		            StandardOpenOption.WRITE);
+		    FileLock lock = fc.tryLock();
+		    
+		    if (lock == null) {
+		        System.out.println("Vous ne pouvez pas lancer plusieurs instances");
+		        System.exit(0);
+		    }
+		} catch (IOException e) {
+		    throw new Error(e);
+		}
+		
+		Application.launch(args);
+	}
+}
